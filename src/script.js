6cd58394bde77fddc51d137fd25ef25c659445c3
JLTC3111 <dr.herbal2011@gmail.com>
2025-04-30 20:57:21 +0700

    update

--- src/script.js
+++ src/script.js
@@ -1,3 +1,7 @@
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 window.attachProfileEvents = () => {
   const profileData = [
     {
