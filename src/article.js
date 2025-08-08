26a11dcc794474d5b2e0745bbd29a7b7ae384f7c
JLTC3111 <dr.herbal2011@gmail.com>
2025-08-09 00:19:06 +0700

    update

--- src/article.js
+++ src/article.js
@@ -1,30 +1,22 @@
 const articles = [
     {
       id: "1",
-      title: "IKI-GIZ-ICUE Completion Ceremony",
-      lead: "Province's Leader Attending",
-      author: "ICUE-IKI-Giz & City of Hội An",
+      title: "Lễ Khánh Thành, Bàn Giao Công Viên Âu Cơ",
+      lead: "Với Sự Tham Dự Của Chủ Tịch UBND Tỉnh Quảng Nam - Ông Lê Văn Dũng",
+      author: "Bởi ICUE-IKI-Giz & Thành Phố Hội An",
       date: "2025-05-16",
       image: {
         src: "/public/news/articles/article_1/all_together.jpg",
-        caption: "Ceremony Participants"
+        caption: "Các Bên Tham Gia"
       },
       bodyHTML: `
-        <p>On May 16, 2025, the Institute for Construction and Urban Economic Research (ICUE), in coordination with the People’s Committee of Hoi An City, organized a special event to inaugurate and hand over the green space and coastal park (now named Au Co Park), marking the successful completion of the project titled:
-        "Preventing erosion on Cua Dai beach through green corridors and park",
-        under the Climate Capacity Building and Biodiversity Action at National and Local Levels (CBF) program.
+        <p>Vào ngày 16 tháng 5 năm 2025, Viện Nghiên Cứu Kinh tế Xây dựng và Đô thị (ICUE), phối hợp cùng UBND thành phố Hội An, tổ chức một sự kiện đặc biệt là Khánh thành và bàn giao Không gian xanh và công viện ven biển (đã được đặt tên là Công viên Âu Cơ), nhằm đánh dấu sự kết thúc thành công của dự án mang tên “Preventing erosion on Cua Dai beach through green corridors and park” nhằmTăng Cường Năng Lực và Hành Động Khí Hậu, đa dạng sinh học ở cấp quốc gia và cấp địa phương (CBF). 
 
-        This initiative was implemented under the grant agreement of the International Climate Initiative (IKI), with ICUE as the grant recipient and project implementer, and the Deutsche Gesellschaft für Internationale Zusammenarbeit (GIZ) GmbH as the project manager. The project played a crucial role in supporting climate action and biodiversity protection efforts in Vietnam.
-
-        The event served not only as a closing ceremony for the project but also as an opportunity to reflect on the progress made thanks to the shared commitment of all partners involved. The presence of stakeholders, experts, and contributors further highlighted the collaborative nature of this initiative and its positive impact on sustainable urban development in the Cua Dai area of Hoi An City.
-
-        Over the past months, the project not only strengthened technical and institutional capacities, but also promoted deeper cooperation between central and local governments on climate change-related issues. ICUE and the Hoi An city government were honored to contribute to this meaningful effort, which reflects a shared vision of a future more resilient to climate change and more responsible toward the environment.
-
-        None of this would have been possible without the generous support from IKI and the enthusiastic assistance from GIZ in implementing the project. We also deeply appreciate the facilitation provided by the People’s Committee of Quang Nam Province, the close coordination with the Hoi An City government, the Cua Dai Ward authorities, and the collaboration from local communities and civil society organizations.
-
-        The trust and funding from IKI & GIZ made this project a reality and delivered tangible benefits to the local community. We'd like to express our heartfelt gratitude to IKI & Giz for their continuous support and the trust they have placed in us.
-
-        This inauguration and handover ceremony is not an end, but rather a new beginning—paving the way for future cooperation towards greener, more sustainable cities in Vietnam and beyond.</p>
+        Sáng kiến này được triển khai theo thỏa thuận tài trợ của Sáng kiến Khí hậu Quốc tế (IKI), ICUE là đơn vị nhận tài trợ và triển khai thực hiện dự án, Tổ chức Hợp tác Phát triển Đức (GIZ) GmbH là đơn vị quản lý dự án. Dự án này đóng vai trò then chốt trong việc hỗ trợ các nỗ lực hành động vì khí hậu và bảo vệ đa dạng sinh học tại Việt Nam. 
+        Sự kiện này vừa là dịp để tổng kết và đóng dự án, vừa là cơ hội để nhìn lại những tiến bộ đạt được nhờ cam kết chung của các đối tác. Sự có mặt của các bên liên quan, các chuyên gia và những người đóng góp cho dự án sẽ càng làm nổi bật tính hợp tác trong sáng kiến này, cũng như tác động tích cực của nó đối với phát triển đô thị bền vững tại khu vực Cửa Đại, thành phố Hội An. 
+        Trong những tháng vừa qua, dự án không chỉ củng cố năng lực kỹ thuật và thể chế mà còn thúc đẩy sự hợp tác sâu sắc hơn giữa chính quyền trung ương và địa phương trong các vấn đề liên quan đến biến đổi khí hậu. ICUE và chính quyền địa phương thành phố Hội An rất vinh dự khi được đóng góp vào nỗ lực đầy ý nghĩa này, phản ánh một tầm nhìn chung về một tương lai thích ứng tốt hơn với biến đổi khí hậu và có trách nhiệm hơn với môi trường. 
+        Tất cả những điều này sẽ không thể thực hiện được nếu không có sự hỗ trợ hào phóng từ IKI và sự hỗ trợ nhiệt tình của GIZ trong việc triển khai thực hiện dự án, cũng như việc tạo điều kiện thuận lợi của UBND tỉnh Quảng Nam, sự phối hợp nhịp nhàng UBND thành phố Hội An, UBND phường Cửa Đại và sự cộng tác của cộng đồng dân cư cũng như các tổ chức xã hội ở đây. 
+        Niềm tin và nguồn tài trợ của họ (IKI và GIZ) đã giúp dự án trở thành hiện thực và mang lại những lợi ích thiết thực cho cộng đồng địa phương. Chúng tôi xin gửi lời cảm ơn chân thành đến GIZ và IKI vì sự hỗ trợ liên tục và niềm tin mà họ dành cho chúng tôi. Sự kiện khánh thành và bàn giao này không phải là kết thúc, mà là một sự khởi đầu mới cho các hợp tác trong tương lai, hướng đến việc xây dựng những đô thị xanh hơn, bền vững hơn tại Việt Nam và xa hơn nữa.</p>
         <h2>Thank You 🤝</h2>
         <blockquote>"We Hope You Enjoyed The Ceremony - Thanks for Coming!"</blockquote>
       `,
