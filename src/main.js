b99d362a65ea9213a929797e3bb0aa244c73eb70
JLTC3111 <dr.herbal2011@gmail.com>
2025-08-20 12:19:12 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -453,7 +453,7 @@ footer {
      function autoInjectFooter() {
         
         const injectIfMissing = () => {
-            const allowedPages = ['#/orgStructure', '#/meetOurExperts', '#/coreTeam', '#/Contact', 'cookies','privacy', 'gdpr', 'terms', 'FAQs', 'recruitment', 'donations' ];
+            const allowedPages = ['#/aboutUs', '#/orgStructure', '#/meetOurExperts', '#/coreTeam', '#/Contact', '#/cookies', '#/privacy', '#/gdpr', '#/terms', '#/FAQs', '#/recruitment', '#/donations'];
             const currentPage = window.location.hash.trim();
             console.log("Current page:", currentPage);
             const normalizedPage = currentPage.replace(/\/$/, '').toLowerCase();
