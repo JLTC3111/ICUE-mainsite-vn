5eff2756e233a854cba4c8c43c9de4331304af56
JLTC3111 <dr.herbal2011@gmail.com>
2025-08-22 13:41:07 +0700

    update

--- src/article.js
+++ src/article.js
@@ -1,6 +1,72 @@
 const articles = [
     {
       id: "1",
+      title: "Lễ Khánh Thành, Bàn Giao Công Viên Âu Cơ",
+      lead: "Với Sự Tham Dự Của Chủ Tịch UBND Tỉnh Quảng Nam - Ông Lê Văn Dũng",
+      author: "Bởi ICUE-IKI-Giz & Thành Phố Hội An",
+      date: "16 Tháng 5, 2025",
+      image: {
+        src: "/public/news/articles/article_1/all_together.jpg",
+        caption: "Các Bên Tham Gia"
+      },
+      bodyHTML: `
+        <p>Vào ngày 16 tháng 5 năm 2025, Viện Nghiên Cứu Kinh tế Xây dựng và Đô thị (ICUE), phối hợp cùng UBND thành phố Hội An, tổ chức một sự kiện đặc biệt là Khánh thành và bàn giao Không gian xanh và công viện ven biển (đã được đặt tên là Công viên Âu Cơ), nhằm đánh dấu sự kết thúc thành công của dự án mang tên “Preventing erosion on Cua Dai beach through green corridors and park” nhằmTăng Cường Năng Lực và Hành Động Khí Hậu, đa dạng sinh học ở cấp quốc gia và cấp địa phương (CBF). 
+
+        Sáng kiến này được triển khai theo thỏa thuận tài trợ của Sáng kiến Khí hậu Quốc tế (IKI), ICUE là đơn vị nhận tài trợ và triển khai thực hiện dự án, Tổ chức Hợp tác Phát triển Đức (GIZ) GmbH là đơn vị quản lý dự án. Dự án này đóng vai trò then chốt trong việc hỗ trợ các nỗ lực hành động vì khí hậu và bảo vệ đa dạng sinh học tại Việt Nam. 
+        Sự kiện này vừa là dịp để tổng kết và đóng dự án, vừa là cơ hội để nhìn lại những tiến bộ đạt được nhờ cam kết chung của các đối tác. Sự có mặt của các bên liên quan, các chuyên gia và những người đóng góp cho dự án sẽ càng làm nổi bật tính hợp tác trong sáng kiến này, cũng như tác động tích cực của nó đối với phát triển đô thị bền vững tại khu vực Cửa Đại, thành phố Hội An. 
+        Trong những tháng vừa qua, dự án không chỉ củng cố năng lực kỹ thuật và thể chế mà còn thúc đẩy sự hợp tác sâu sắc hơn giữa chính quyền trung ương và địa phương trong các vấn đề liên quan đến biến đổi khí hậu. ICUE và chính quyền địa phương thành phố Hội An rất vinh dự khi được đóng góp vào nỗ lực đầy ý nghĩa này, phản ánh một tầm nhìn chung về một tương lai thích ứng tốt hơn với biến đổi khí hậu và có trách nhiệm hơn với môi trường. 
+        Tất cả những điều này sẽ không thể thực hiện được nếu không có sự hỗ trợ hào phóng từ IKI và sự hỗ trợ nhiệt tình của GIZ trong việc triển khai thực hiện dự án, cũng như việc tạo điều kiện thuận lợi của UBND tỉnh Quảng Nam, sự phối hợp nhịp nhàng UBND thành phố Hội An, UBND phường Cửa Đại và sự cộng tác của cộng đồng dân cư cũng như các tổ chức xã hội ở đây. 
+        Niềm tin và nguồn tài trợ của họ (IKI và GIZ) đã giúp dự án trở thành hiện thực và mang lại những lợi ích thiết thực cho cộng đồng địa phương. Chúng tôi xin gửi lời cảm ơn chân thành đến GIZ và IKI vì sự hỗ trợ liên tục và niềm tin mà họ dành cho chúng tôi. Sự kiện khánh thành và bàn giao này không phải là kết thúc, mà là một sự khởi đầu mới cho các hợp tác trong tương lai, hướng đến việc xây dựng những đô thị xanh hơn, bền vững hơn tại Việt Nam và xa hơn nữa.</p>
+        <h2>Xin Chân Thành Cám Ơn!</h2>
+        <blockquote>"Xin kính chúc quý vị đại biểu có nhiều Sức Khỏe - An Vui - Hạnh Phúc - và Thành Công. - T.S. Nguyễn Hồng Hạnh"</blockquote>
+      `,
+      pdf: "/public/files/speech.pdf",
+      pdfButtonText: "Tải Về - Bài Phát Biểu ⇲"
+    },
+    {
+      id: "2",
+      title: "Chung tay đóng góp, ủng hộ, giúp đỡ đồng bào ảnh hưởng do bão Yagi",
+      lead: "Làm theo lời kêu gọi của Uỷ ban Trung ương Mặt trận Tổ quốc Việt Nam, Viện NCKTXD&ĐT đã có thông báo kêu gọi cán bộ và các đối tác cùng các nhà hảo tâm chung tay đóng góp, giúp đỡ đồng bào bị ảnh hưởng bởi bão Yagi.",
+      author: "Bởi ICUE",
+      date: "26 Tháng 9, 2024",
+      image: {
+        src: "/public/news/articles/article_3/area_affected.png",
+        caption: "Nhóm Tình Nguyện (ICUE)"
+      },
+      bodyHTML: `
+        <p>Trong những ngày qua, do ảnh hưởng của cơn bão số 3 (bão Yagi), trên địa bản huyện Bảo Yên liên tục hứng chịu mưa lớn, lũ chồng lũ khiến nhiều xã trong huyện bị thiệt hại nặng nề. Đặc biệt trong 03 ngày: Từ 08-10/9/2024 mưa lớn kéo dài cùng nước nước lũ dâng cao, gây ngập úng, sạt lở đất đá nhiều nơi. Mưa lũ, sạt lở đất đá đến thời điểm hiện tại đã có 71 người chết, 29 người bị thương và chưa xác định được 11 người; hệ thống giao thông hư hỏng nghiêm trọng; nhà cửa, tài sản, hoa màu bị thiệt hại nặng nề, nhiều nhà bị mất trắng (đã có 4.825 nhà bị ảnh hưởng, thiệt hại khoảng 820 tỷ đồng). Đây là đợt lũ lụt lớn chưa từng thấy trên địa bàn huyện Bảo Yên. Ngày 25/9/2024, Đoàn cứu trợ Viện ICUE cùng các nhà hảo tâm đã thực hiện chuyến đi nghĩa tình hướng về bà con huyện  Bảo Yên. Theo sự điều phối, hướng dẫn của ban tiếp nhận UBND, UBMTTQ VN huyện Bảo Yên do đồng chí Đoàn Xuân Hưng chỉ đạo đã hướng dẫn Đoàn tới bản Chom – xã Yên Sơn để trao 100 phần quà tới tay bà con. Mỗi phần quà bao gồm: 10kg gạo đài thơm, dầu ăn, lạc rang sẵn, thịt chưng mắm tép, bột canh Hải Châu… cùng với quần áo, chăn màn. Tại bản Chom thiệt hại nhiều về tài sản, hoa màu, gia súc, gia cầm,… trong đó có 03 hộ gia đình bị sập đổ hoàn toàn nhưng may mắn không có thiệt hại về người gồm có gia đình: Bà Hoàng Thị Bốn, Ông Hoàng Văn Bản, Ông Nguyễn Bá Quán – được trao số quà gấp 4 lần các hộ khác.</p>
+        <h2></h2>
+        <blockquote>"Mong bà con sớm ổn định cuộc sống, vượt qua khó khăn, chung tay xây dựng và phát triển vững mạnh"</blockquote>
+      `,
+      pdf: "/public/files/photos.zip",
+      pdfButtonText: "Tải Về - Ảnh Tư Liệu ⇲"
+    },
+    {
+      id: "3",
+      title: "Khai mạc Diễn đàn Bảo tồn Khu vực Châu Á lần thứ 8 tại Thái Lan",
+      lead: "Ngày 3/9, Diễn đàn Bảo tồn Khu vực Châu Á (RCF) lần thứ 8 của Liên minh Bảo tồn Thiên nhiên Quốc tế (IUCN) đã khai mạc tại Bangkok, Thái Lan. Sự kiện quy tụ gần 600 nhà lãnh đạo trong lĩnh vực bảo tồn từ khắp khu vực, bao gồm đại diện chính phủ, tổ chức phi chính phủ, nhà tài trợ và đối tác, học viện và khu vực tư nhân, cùng nhiều bên liên quan.",
+      author: "Bởi ICUE",
+      date: "03 Tháng 9, 2024",
+      image: {
+        src: "/public/news/articles/article_2/conference.png",
+        caption: "Phái đoàn Việt Nam tham gia hoạt động trong khuôn khổ RCF 2024"
+      },
+      bodyHTML: `
+        <p>Về phía Việt Nam, đại diện phái đoàn tham dự Diễn đàn có các thành viên của IUCN Việt Nam, trong đó đại diện Viện Nghiên cứu Kinh tế Xây dựng và Đô thị (ICUE) - Viện trưởng TS.Nguyễn Hồng Hạnh tham dự diễn đàn. ICUE là 1 trong các thành viên đã tham dự và có các trình bày về bảo tồn biển, phát triển bền vững nông thôn. ICUE đã có báo cáo tóm tắt một phần trong dự án "Hỗ trợ phòng chống xói lở bờ biển Cửa Đại thông qua hành lang xanh và công viên sinh thái ven biển". 
+        Diễn đàn Bảo tồn Khu vực Châu Á (RCF) diễn ra trong ba ngày với chủ đề 'Tái hiện Bảo tồn tại Châu Á: Tương lai tích cực cho Thiên nhiên', hướng tới đánh giá tiến độ bảo tồn, xem xét lại các mục tiêu ưu tiên và đề xuất các định hướng chiến lược để giải quyết hiệu quả các thách thức về môi trường và đa dạng sinh học trong 20 năm tới.
+        Trong khuôn khổ RCF 2024, Diễn đàn Lãnh đạo Thanh niên đầu tiên, do thanh niên từ 23 quốc gia tổ chức, hướng tới nhấn mạnh vai trò của chuyên gia trẻ cũng như đóng góp ngày càng tăng của họ cho công tác bảo tồn thiên nhiên.
+        Bên cạnh các cuộc thảo luận cấp cao về những thách thức trong khu vực của nhiều bên liên quan, IUCN Châu Á RCF lần thứ 8 sẽ có tám phiên họp chuyên đề về các ưu tiên chương trình mới và hiện có, cũng như 17 sự kiện bên lề do các Thành viên, Ủy ban và đối tác của IUCN tổ chức. Địa điểm này cũng cung cấp các gian hàng triển lãm để giới thiệu công tác bảo tồn.
+        TS.Nguyễn Hồng Hạnh cùng chuyên gia Pornphrom Vikitsreth là nhà phân tích chính sách tại đảng Dân chủ Thái Lan và là người ủng hộ mạnh mẽ các chương trình nghị sự về biến đổi khí hậu của đảng, sử dụng cả các phương pháp giảm thiểu và thích ứng. Ông cũng đã nâng cao nhận thức về các vấn đề biến đổi khí hậu trong các mạng lưới thanh niên và cộng đồng địa phương trên khắp đất nước và có bằng Thạc sĩ về các vấn đề toàn cầu của Đại học New York.
+        Ngoài ra còn kết nối với Viện Môi trường Thái Lan (Thailand Environment Instutite) Trở thành tổ chức hàng đầu về môi trường theo tiêu chuẩn quốc tế, tuân thủ nguyên tắc phi đảng phái , góp phần thúc đẩy phát triển bền vững. 
+        CBCGDF là một quỹ gây quỹ công cộng quốc gia tại Trung Quốc. Trong những năm qua, quỹ đã đóng vai trò quan trọng trong lĩnh vực bảo tồn đa dạng sinh học và phát triển xanh. Để thích ứng với sự phát triển của thời đại, quỹ đã được đổi tên thành “Quỹ bảo tồn đa dạng sinh học và phát triển xanh Trung Quốc”. Việc đổi tên đã kết hợp hữu cơ bảo tồn đa dạng sinh học với phát triển xanh, mở rộng hàm ý và biểu thị của bảo tồn đa dạng sinh học, thể hiện bản chất của đa dạng sinh học “phát triển trong bảo tồn và được bảo tồn trong phát triển”, và đóng vai trò tích cực trong việc thúc đẩy tái cấu trúc kinh tế tại Trung Quốc.    
+        Trong sự kiện kéo dài ba ngày, một sự kiện học tập chuyên dụng sẽ được tổ chức, bao gồm các buổi chia sẻ kiến ​​thức và đào tạo ngắn hạn do Học viện IUCN tổ chức. Những người tham gia cũng sẽ có cơ hội dự triển lãm, tham khảo nhiều nỗ lực hợp tác khác nhau để bảo tồn thiên nhiên và đa dạng sinh học.</p>
+        <h2></h2>
+        <blockquote></blockquote>
+      `,
+      pdf: ""
+    },
+    {
+      id: "4",
       title: "Hội nghị tổng kết đề án phát triển đô thị thông minh và bền vững VN giai đoạn 2018-2025 vag định hướng 2030",
       lead: "Sáng Kiến Đô Thị Thông Minh của Việt Nam: Thành Tựu và Lộ Trình 2025-2030",
       author: "Bởi ICUE",
@@ -47,72 +113,6 @@ const articles = [
       pdf: "",
       pdfButtonText: ""
     },
-    {
-      id: "2",
-      title: "Lễ Khánh Thành, Bàn Giao Công Viên Âu Cơ",
-      lead: "Với Sự Tham Dự Của Chủ Tịch UBND Tỉnh Quảng Nam - Ông Lê Văn Dũng",
-      author: "Bởi ICUE-IKI-Giz & Thành Phố Hội An",
-      date: "16 Tháng 5, 2025",
-      image: {
-        src: "/public/news/articles/article_1/all_together.jpg",
-        caption: "Các Bên Tham Gia"
-      },
-      bodyHTML: `
-        <p>Vào ngày 16 tháng 5 năm 2025, Viện Nghiên Cứu Kinh tế Xây dựng và Đô thị (ICUE), phối hợp cùng UBND thành phố Hội An, tổ chức một sự kiện đặc biệt là Khánh thành và bàn giao Không gian xanh và công viện ven biển (đã được đặt tên là Công viên Âu Cơ), nhằm đánh dấu sự kết thúc thành công của dự án mang tên “Preventing erosion on Cua Dai beach through green corridors and park” nhằmTăng Cường Năng Lực và Hành Động Khí Hậu, đa dạng sinh học ở cấp quốc gia và cấp địa phương (CBF). 
-
-        Sáng kiến này được triển khai theo thỏa thuận tài trợ của Sáng kiến Khí hậu Quốc tế (IKI), ICUE là đơn vị nhận tài trợ và triển khai thực hiện dự án, Tổ chức Hợp tác Phát triển Đức (GIZ) GmbH là đơn vị quản lý dự án. Dự án này đóng vai trò then chốt trong việc hỗ trợ các nỗ lực hành động vì khí hậu và bảo vệ đa dạng sinh học tại Việt Nam. 
-        Sự kiện này vừa là dịp để tổng kết và đóng dự án, vừa là cơ hội để nhìn lại những tiến bộ đạt được nhờ cam kết chung của các đối tác. Sự có mặt của các bên liên quan, các chuyên gia và những người đóng góp cho dự án sẽ càng làm nổi bật tính hợp tác trong sáng kiến này, cũng như tác động tích cực của nó đối với phát triển đô thị bền vững tại khu vực Cửa Đại, thành phố Hội An. 
-        Trong những tháng vừa qua, dự án không chỉ củng cố năng lực kỹ thuật và thể chế mà còn thúc đẩy sự hợp tác sâu sắc hơn giữa chính quyền trung ương và địa phương trong các vấn đề liên quan đến biến đổi khí hậu. ICUE và chính quyền địa phương thành phố Hội An rất vinh dự khi được đóng góp vào nỗ lực đầy ý nghĩa này, phản ánh một tầm nhìn chung về một tương lai thích ứng tốt hơn với biến đổi khí hậu và có trách nhiệm hơn với môi trường. 
-        Tất cả những điều này sẽ không thể thực hiện được nếu không có sự hỗ trợ hào phóng từ IKI và sự hỗ trợ nhiệt tình của GIZ trong việc triển khai thực hiện dự án, cũng như việc tạo điều kiện thuận lợi của UBND tỉnh Quảng Nam, sự phối hợp nhịp nhàng UBND thành phố Hội An, UBND phường Cửa Đại và sự cộng tác của cộng đồng dân cư cũng như các tổ chức xã hội ở đây. 
-        Niềm tin và nguồn tài trợ của họ (IKI và GIZ) đã giúp dự án trở thành hiện thực và mang lại những lợi ích thiết thực cho cộng đồng địa phương. Chúng tôi xin gửi lời cảm ơn chân thành đến GIZ và IKI vì sự hỗ trợ liên tục và niềm tin mà họ dành cho chúng tôi. Sự kiện khánh thành và bàn giao này không phải là kết thúc, mà là một sự khởi đầu mới cho các hợp tác trong tương lai, hướng đến việc xây dựng những đô thị xanh hơn, bền vững hơn tại Việt Nam và xa hơn nữa.</p>
-        <h2>Xin Chân Thành Cám Ơn!</h2>
-        <blockquote>"Xin kính chúc quý vị đại biểu có nhiều Sức Khỏe - An Vui - Hạnh Phúc - và Thành Công. - T.S. Nguyễn Hồng Hạnh"</blockquote>
-      `,
-      pdf: "/public/files/speech.pdf",
-      pdfButtonText: "Tải Về - Bài Phát Biểu ⇲"
-    },
-    {
-      id: "3",
-      title: "Chung tay đóng góp, ủng hộ, giúp đỡ đồng bào ảnh hưởng do bão Yagi",
-      lead: "Làm theo lời kêu gọi của Uỷ ban Trung ương Mặt trận Tổ quốc Việt Nam, Viện NCKTXD&ĐT đã có thông báo kêu gọi cán bộ và các đối tác cùng các nhà hảo tâm chung tay đóng góp, giúp đỡ đồng bào bị ảnh hưởng bởi bão Yagi.",
-      author: "Bởi ICUE",
-      date: "26 Tháng 9, 2024",
-      image: {
-        src: "/public/news/articles/article_3/area_affected.png",
-        caption: "Nhóm Tình Nguyện (ICUE)"
-      },
-      bodyHTML: `
-        <p>Trong những ngày qua, do ảnh hưởng của cơn bão số 3 (bão Yagi), trên địa bản huyện Bảo Yên liên tục hứng chịu mưa lớn, lũ chồng lũ khiến nhiều xã trong huyện bị thiệt hại nặng nề. Đặc biệt trong 03 ngày: Từ 08-10/9/2024 mưa lớn kéo dài cùng nước nước lũ dâng cao, gây ngập úng, sạt lở đất đá nhiều nơi. Mưa lũ, sạt lở đất đá đến thời điểm hiện tại đã có 71 người chết, 29 người bị thương và chưa xác định được 11 người; hệ thống giao thông hư hỏng nghiêm trọng; nhà cửa, tài sản, hoa màu bị thiệt hại nặng nề, nhiều nhà bị mất trắng (đã có 4.825 nhà bị ảnh hưởng, thiệt hại khoảng 820 tỷ đồng). Đây là đợt lũ lụt lớn chưa từng thấy trên địa bàn huyện Bảo Yên. Ngày 25/9/2024, Đoàn cứu trợ Viện ICUE cùng các nhà hảo tâm đã thực hiện chuyến đi nghĩa tình hướng về bà con huyện  Bảo Yên. Theo sự điều phối, hướng dẫn của ban tiếp nhận UBND, UBMTTQ VN huyện Bảo Yên do đồng chí Đoàn Xuân Hưng chỉ đạo đã hướng dẫn Đoàn tới bản Chom – xã Yên Sơn để trao 100 phần quà tới tay bà con. Mỗi phần quà bao gồm: 10kg gạo đài thơm, dầu ăn, lạc rang sẵn, thịt chưng mắm tép, bột canh Hải Châu… cùng với quần áo, chăn màn. Tại bản Chom thiệt hại nhiều về tài sản, hoa màu, gia súc, gia cầm,… trong đó có 03 hộ gia đình bị sập đổ hoàn toàn nhưng may mắn không có thiệt hại về người gồm có gia đình: Bà Hoàng Thị Bốn, Ông Hoàng Văn Bản, Ông Nguyễn Bá Quán – được trao số quà gấp 4 lần các hộ khác.</p>
-        <h2></h2>
-        <blockquote>"Mong bà con sớm ổn định cuộc sống, vượt qua khó khăn, chung tay xây dựng và phát triển vững mạnh"</blockquote>
-      `,
-      pdf: "/public/files/photos.zip",
-      pdfButtonText: "Tải Về - Ảnh Tư Liệu ⇲"
-    },
-    {
-      id: "4",
-      title: "Khai mạc Diễn đàn Bảo tồn Khu vực Châu Á lần thứ 8 tại Thái Lan",
-      lead: "Ngày 3/9, Diễn đàn Bảo tồn Khu vực Châu Á (RCF) lần thứ 8 của Liên minh Bảo tồn Thiên nhiên Quốc tế (IUCN) đã khai mạc tại Bangkok, Thái Lan. Sự kiện quy tụ gần 600 nhà lãnh đạo trong lĩnh vực bảo tồn từ khắp khu vực, bao gồm đại diện chính phủ, tổ chức phi chính phủ, nhà tài trợ và đối tác, học viện và khu vực tư nhân, cùng nhiều bên liên quan.",
-      author: "Bởi ICUE",
-      date: "03 Tháng 9, 2024",
-      image: {
-        src: "/public/news/articles/article_2/conference.png",
-        caption: "Phái đoàn Việt Nam tham gia hoạt động trong khuôn khổ RCF 2024"
-      },
-      bodyHTML: `
-        <p>Về phía Việt Nam, đại diện phái đoàn tham dự Diễn đàn có các thành viên của IUCN Việt Nam, trong đó đại diện Viện Nghiên cứu Kinh tế Xây dựng và Đô thị (ICUE) - Viện trưởng TS.Nguyễn Hồng Hạnh tham dự diễn đàn. ICUE là 1 trong các thành viên đã tham dự và có các trình bày về bảo tồn biển, phát triển bền vững nông thôn. ICUE đã có báo cáo tóm tắt một phần trong dự án "Hỗ trợ phòng chống xói lở bờ biển Cửa Đại thông qua hành lang xanh và công viên sinh thái ven biển". 
-        Diễn đàn Bảo tồn Khu vực Châu Á (RCF) diễn ra trong ba ngày với chủ đề 'Tái hiện Bảo tồn tại Châu Á: Tương lai tích cực cho Thiên nhiên', hướng tới đánh giá tiến độ bảo tồn, xem xét lại các mục tiêu ưu tiên và đề xuất các định hướng chiến lược để giải quyết hiệu quả các thách thức về môi trường và đa dạng sinh học trong 20 năm tới.
-        Trong khuôn khổ RCF 2024, Diễn đàn Lãnh đạo Thanh niên đầu tiên, do thanh niên từ 23 quốc gia tổ chức, hướng tới nhấn mạnh vai trò của chuyên gia trẻ cũng như đóng góp ngày càng tăng của họ cho công tác bảo tồn thiên nhiên.
-        Bên cạnh các cuộc thảo luận cấp cao về những thách thức trong khu vực của nhiều bên liên quan, IUCN Châu Á RCF lần thứ 8 sẽ có tám phiên họp chuyên đề về các ưu tiên chương trình mới và hiện có, cũng như 17 sự kiện bên lề do các Thành viên, Ủy ban và đối tác của IUCN tổ chức. Địa điểm này cũng cung cấp các gian hàng triển lãm để giới thiệu công tác bảo tồn.
-        TS.Nguyễn Hồng Hạnh cùng chuyên gia Pornphrom Vikitsreth là nhà phân tích chính sách tại đảng Dân chủ Thái Lan và là người ủng hộ mạnh mẽ các chương trình nghị sự về biến đổi khí hậu của đảng, sử dụng cả các phương pháp giảm thiểu và thích ứng. Ông cũng đã nâng cao nhận thức về các vấn đề biến đổi khí hậu trong các mạng lưới thanh niên và cộng đồng địa phương trên khắp đất nước và có bằng Thạc sĩ về các vấn đề toàn cầu của Đại học New York.
-        Ngoài ra còn kết nối với Viện Môi trường Thái Lan (Thailand Environment Instutite) Trở thành tổ chức hàng đầu về môi trường theo tiêu chuẩn quốc tế, tuân thủ nguyên tắc phi đảng phái , góp phần thúc đẩy phát triển bền vững. 
-        CBCGDF là một quỹ gây quỹ công cộng quốc gia tại Trung Quốc. Trong những năm qua, quỹ đã đóng vai trò quan trọng trong lĩnh vực bảo tồn đa dạng sinh học và phát triển xanh. Để thích ứng với sự phát triển của thời đại, quỹ đã được đổi tên thành “Quỹ bảo tồn đa dạng sinh học và phát triển xanh Trung Quốc”. Việc đổi tên đã kết hợp hữu cơ bảo tồn đa dạng sinh học với phát triển xanh, mở rộng hàm ý và biểu thị của bảo tồn đa dạng sinh học, thể hiện bản chất của đa dạng sinh học “phát triển trong bảo tồn và được bảo tồn trong phát triển”, và đóng vai trò tích cực trong việc thúc đẩy tái cấu trúc kinh tế tại Trung Quốc.    
-        Trong sự kiện kéo dài ba ngày, một sự kiện học tập chuyên dụng sẽ được tổ chức, bao gồm các buổi chia sẻ kiến ​​thức và đào tạo ngắn hạn do Học viện IUCN tổ chức. Những người tham gia cũng sẽ có cơ hội dự triển lãm, tham khảo nhiều nỗ lực hợp tác khác nhau để bảo tồn thiên nhiên và đa dạng sinh học.</p>
-        <h2></h2>
-        <blockquote></blockquote>
-      `,
-      pdf: ""
-    },
 ];
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -132,7 +132,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.title = article.title;
     document.getElementById("article-title").textContent = article.title;
     document.getElementById("article-lead").textContent = article.lead;
-    document.getElementById("article-author").textContent = `By ${article.author}`;
+    document.getElementById("article-author").textContent = `${article.author}`;
     document.getElementById("article-date").textContent = article.date;
     document.getElementById("article-date").setAttribute("datetime", article.date);
     document.getElementById("article-image").src = article.image.src;
